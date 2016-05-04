0d43fcee7277b805be1dcc90a4f5d849955e8b2b
Strachan, Alex <astrachan@conversantmedia.com>
2016-05-04 10:05:29 -0700

    add basic test cases

--- tests.js
+++ tests.js
@@ -1,5 +1,36 @@
 var templateparser = require('./templatestring.js');
 
-var template = "hi ${name}, I'm ${module.name}"
-var obj = {name: "User", module: {name: "templateparser", description: "foo"}};
-console.log(templateparser(template, obj));
+var tests = {
+    "one-layer object":{
+        str: "hi ${foo}",
+        obj: {foo: "bar"},
+        expect: "hi bar"
+    },
+    "two-layer object":{
+        str: "hi ${foo.bar}",
+        obj: {foo: {bar: "whack"}},
+        expect: "hi whack"
+    },
+    "null string test":{
+        str: undefined,
+        obj: {foo: "bar"},
+        expect: ""
+    },
+    "null obj test":{
+        str: "foo",
+        obj: undefined,
+        expect: "foo"
+    }
+}
+
+
+for(var key in tests) {
+    str = tests[key].str;
+    obj = tests[key].obj;
+    expect = tests[key].expect
+    if(templateparser(str, obj) !== expect) {
+        throw new Error(["test", key, "failed"].join(" "))
+    }
+}
+
+console.log("tests pass")
