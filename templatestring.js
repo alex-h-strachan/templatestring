2ddc65e85699aa319a83d9d71be440754f2f93d4
Strachan, Alex <astrachan@conversantmedia.com>
2016-05-04 13:46:11 -0700

    fix keys with $s in the string by targeting more carefully in the regex.

--- templatestring.js
+++ templatestring.js
@@ -1,7 +1,9 @@
+"use strict";
+
 module.exports = function(str, obj) {
     str = str || "";
     return str.replace(/\$\{[^}]+\}/g, function(match){
-        var keyArray = match.replace(/[\{\}\$]/g, "").split(".");
+        var keyArray = match.replace(/^\$\{|}$/g, "").split(".");
         var currentVal = obj;
         for(var i = 0; i < keyArray.length; i++) {
             currentVal = currentVal[keyArray[i]];
